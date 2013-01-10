4cdc26e800da24bb608d86d7c28b6544040d59e1
Liviu Ionescu <ilg@livius.net>
2013-01-11 00:38:10 +0200

    basePath removed; buildSubFolder added; buildIncludeFolders currently as absolute

--- packages/meta/xcdl.py
+++ packages/meta/xcdl.py
@@ -5,24 +5,25 @@ Package(
     name='The µOS++ SE repository',
     description='All µOS++ Second Edition components are below this node.',
 
+    # Mark this as the root of the component repository (informative).
     category='root',
-            
-    # The below files are relative to the root of the packages repository.
-
-    # Since the config scripts are located in the 'meta' subfolder, we need to 
-    # define how to compute the package root folder, starting from the current 
-    # subfolder (one step up in the hierarchy).
-    # This value is inherited by all children.
-    basePath='..',
     
     # The list of possible relative paths where the sources can be located, 
     # starting from package root folder.
     # This value is inherited by all children.
     sourcesPaths=['src', '.'],
     
-    # Each component tree will generate a subfolder below the build folder
+    # Each component tree will generate a subfolder below the build folder.
     buildSubFolder='micro-os-plus',
     
+    # The default dynamically generated header file, where all definitions that 
+    # call for inclusions must be created.
     headerFile='include/xcdl/OS_Defines.h',
+    
+    # Each repository must contribute an include path to it's root.
+    # The second definition is related to the previous 'headerFile', and 
+    # allows the compiler to locate the dynamically generated headers.
+    #buildIncludeFolders=['$(REPO)', '$(BUILD)/include'],
+    buildIncludeFolders=['/Users/ilg/My Files/MacBookPro Projects/uOS/micro-os-plus-se.git/packages', '/Users/ilg/tmp/build/include'],
     
 )
