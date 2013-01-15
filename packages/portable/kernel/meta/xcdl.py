0d646b056d323ffaf12dc519347a5e0438d8724c
Liviu Ionescu <ilg@livius.net>
2013-01-15 22:30:28 +0200

    option.os.portable.kernel.debug option added to generate the DEBUG definition

--- packages/portable/kernel/meta/xcdl.py
+++ packages/portable/kernel/meta/xcdl.py
@@ -1,5 +1,8 @@
 # -*- coding: utf-8 -*-
 
+# Copyright (C) 2013 Liviu Ionescu.
+# This file is part of the uOS++ SE distribution.
+
 Package(
     id='package.os.portable.kernel',
     name='The µOS++ SE kernel',
@@ -11,5 +14,17 @@ Package(
         
     includes=[
         'greeting.py',
+    ],
+        
+    children=[
+        Option(
+            id='option.os.portable.kernel.debug',
+            name='The main debug swich',
+            description='Should be enabled for the debug output to be active.',
+            
+            isEnabled=False,
+            
+            headerDefinition='DEBUG',
+        ),
     ],
 )
