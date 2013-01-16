6f6b80cb51cc2a50709488afb8e876884c50b2ae
Liviu Ionescu <ilg@livius.net>
2013-01-16 20:18:52 +0200

    MacPorts gcc46 and gcc47 toolchains added

--- packages/portable/devices/debug/tests/conversions/meta/config.py
+++ packages/portable/devices/debug/tests/conversions/meta/config.py
@@ -49,7 +49,7 @@ Configuration(
                     name='Release OS X sjlj conversion test configuration',
                     description='Release build configuration for conversions',
                     
-                    buildFolder='$(PARENT)/Release',
+                    buildFolder='$(PARENT)/llvm/Release',
                     
                     toolchain='toolchain.osx.llvm.release',
                 ),
@@ -59,7 +59,7 @@ Configuration(
                     name='Debug OS X sjlj conversion test configuration',
                     description='Debug build configuration for conversions',
                     
-                    buildFolder='$(PARENT)/Debug',
+                    buildFolder='$(PARENT)/llvm/Debug',
                     
                     requires=[
                         'enable("DEBUG")',
@@ -71,6 +71,35 @@ Configuration(
                     #],
                     toolchain='toolchain.osx.llvm.debug',
                 ),
+
+                Configuration(
+                    id='config.os.portable.devices.debug.tests.conversions.osx.cl-sjlj.gcc46.debug',
+                    name='Debug OS X sjlj conversion test configuration with GCC 4.6',
+                    description='Debug build configuration for conversions using GNU GCC 4.6',
+                    
+                    buildFolder='$(PARENT)/gcc46/Debug',
+                    
+                    requires=[
+                        'enable("DEBUG")',
+                    ],
+                      
+                    toolchain='toolchain.osx.mp.gcc46.debug',
+                ),
+
+                Configuration(
+                    id='config.os.portable.devices.debug.tests.conversions.osx.cl-sjlj.gcc47.debug',
+                    name='Debug OS X sjlj conversion test configuration with GCC 4.7',
+                    description='Debug build configuration for conversions using GNU GCC 4.7',
+                    
+                    buildFolder='$(PARENT)/gcc47/Debug',
+                    
+                    requires=[
+                        'enable("DEBUG")',
+                    ],
+                      
+                    toolchain='toolchain.osx.mp.gcc47.debug',
+                ),
+
             ],
         ),
     ],
--- packages/support/toolchains/xcdl.py
+++ packages/support/toolchains/xcdl.py
@@ -14,8 +14,8 @@ Package(
 
 Toolchain(
     id='toolchain.osx.llvm.base',
-    name='OS X LLVM x86_64 Base',
-    description='The default Mac OS X Apple LLVM x86_64 toolchain base',
+    name='OS X Apple x86_64 LLVM Base',
+    description='The Apple OS X x86_64 LLVM toolchain base',
     
     category='base',
     parent='package.os.toolchains.base',
@@ -37,33 +37,33 @@ Toolchain(
     
     cc=Tool(
         programName='clang',
-        description='LLVM Clang',
+        description='Apple LLVM Clang',
         options='',
     ),
           
     cpp=Tool(
         programName='clang++',
-        description='LLVM Clang++',    
+        description='Apple LLVM Clang++',    
         options='',
     ),
           
     asm=Tool(
         programName='clang',
-        description='LLVM Clang',
+        description='Apple LLVM Clang',
         options='',
     ),
 
     ld=Tool(
         programName='clang++',
-        description='LLVM Clang++',    
+        description='Apple LLVM Clang++',    
     ),
     
     children=[
               
         Toolchain(
             id='toolchain.osx.llvm.debug',
-            name='OS X LLVM x86_64 Debug',
-            description='The default Mac OS X Apple LLVM x86_64 toolchain for debug builds',
+            name='OS X Apple x86_64 LLVM Debug',
+            description='The Apple OS X x86_64 LLVM toolchain for debug builds',
             
             category='debug',
             
@@ -73,13 +73,120 @@ Toolchain(
 
         Toolchain(
             id='toolchain.osx.llvm.release',
-            name='OS X LLVM x86_64 Release',
-            description='The default Mac OS X Apple LLVM x86_64 toolchain for release builds',
+            name='OS X Apple x86_64 LLVM Release',
+            description='The Apple OS X x86_64 LLVM toolchain for release builds',
             
             category='release',
             
-            # use an agressive optimisation level
+            # use an aggressive optimisation level
             compilerOptimisationOptions='-O3',
         ),              
+    ],
+)
+
+
+Toolchain(
+    id='toolchain.osx.mp.gcc.base',
+    name='OS X MacPorts x86_64 GCC Base',
+    description='The OS X MacPorts x86_64 GCC toolchain base',
+    
+    category='base',
+    parent='package.os.toolchains.base',
+    
+    platformSystem='Darwin',
+
+    programNamePrefix='x86_64-apple-darwin12-',
+    
+    compilerObjectsExtension='o',
+    makeObjectsVariable='OBJS',
+    
+    compilerDepsOptions='-MMD -MP',
+    compilerOutputOptions='-o "$@"',
+    compilerInputOptions='"$<"',
+    compilerWarningOptions='-Wall',
+    
+    compilerMiscOptions='-fsigned-char -fmessage-length=0 -c',
+    compilerDebugOptions='-g3',
+    compilerOptimisationOptions='-O0',
+    
+    linkerMiscOptions='',
+    
+    cc=Tool(
+        programName='$(PREFIX)gcc$(SUFFIX)',
+        description='MacPorts GNU GCC',
+        options='',
+    ),
+          
+    cpp=Tool(
+        programName='$(PREFIX)g++$(SUFFIX)',
+        description='MacPorts GNU G++',    
+        options='',
+    ),
+          
+    asm=Tool(
+        programName='$(PREFIX)gcc$(SUFFIX)',
+        description='MacPorts GNU GCC',
+        options='',
+    ),
+
+    ld=Tool(
+        programName='$(PREFIX)g++$(SUFFIX)',
+        description='MacPorts GNU G++',    
+    ),
+    
+    children=[
+              
+        Toolchain(
+            id='toolchain.osx.mp.gcc46.debug',
+            name='OS X MacPorts x86_64 GCC 4.6 Debug',
+            description='The OS X MacPorts x86_64 GCC 4.6 toolchain for debug builds',
+            
+            category='debug',
+            
+            programNameSuffix='-mp-4.6',
+            
+            # to keep gdb happy, disable optimisations
+            compilerOptimisationOptions='-O0',
+        ),
+
+        Toolchain(
+            id='toolchain.osx.mp.gcc46.release',
+            name='OS X Mac Ports x86_64 GCC 4.6 Release',
+            description='The OS X MacPorts x86_64 GCC 4.6 toolchain for release builds',
+            
+            category='debug',
+            
+            programNameSuffix='-mp-4.6',
+            
+            # use an aggressive optimisation level
+            compilerOptimisationOptions='-O3',
+        ),
+
+        Toolchain(
+            id='toolchain.osx.mp.gcc47.debug',
+            name='OS X Mac Ports x86_64 GCC 4.7 Debug',
+            description='The OS X MacPorts x86_64 GCC 4.7 toolchain for debug builds',
+            
+            category='debug',
+            
+            programNameSuffix='-mp-4.7',
+            
+            # to keep gdb happy, disable optimisations
+            compilerOptimisationOptions='-O0',
+        ),
+
+        Toolchain(
+            id='toolchain.osx.mp.gcc47.release',
+            name='OS X Mac Ports x86_64 GCC 4.7.2 Release',
+            description='The OS X MacPorts x86_64 GCC 4.7 toolchain for release builds',
+            
+            category='debug',
+            
+            programNameSuffix='-mp-4.7',
+            
+            # use an aggressive optimisation level
+            compilerOptimisationOptions='-O3',
+        ),
+    
     ],
 )
