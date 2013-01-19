7b8d0fd0567544b2e1de29f169e5566832f21925
Liviu Ionescu <ilg@livius.net>
2013-01-19 16:31:28 +0200

    most Linux x64/x32 toolchains defined; Darwin x32 toolchains defined

--- packages/support/toolchains/xcdl.py
+++ packages/support/toolchains/xcdl.py
@@ -13,11 +13,13 @@ Package(
 
 
 Toolchain(
+          
     id='toolchain.osx.llvm.base',
-    name='OS X Apple x86_64 LLVM Base',
-    description='The Apple OS X x86_64 LLVM toolchain base',
+    name='OS X Apple LLVM Base',
+    description='The Apple OS X LLVM toolchain base',
     
     category='base',
+    
     parent='package.os.toolchains.base',
     
     platformSystem='Darwin',
@@ -61,31 +63,83 @@ Toolchain(
     children=[
               
         Toolchain(
-            id='toolchain.osx.llvm.debug',
-            name='OS X Apple x86_64 LLVM Debug',
-            description='The Apple OS X x86_64 LLVM toolchain for debug builds',
+                  
+            id='toolchain.osx.x64.llvm.base',
+            name='OS X Apple x86_64 LLVM Base',
+            description='The Apple OS X x86_64 LLVM toolchain base',
             
-            category='debug',
+            category='base',
+
+            compilerArchitecture='-arch=x86_64',
             
-            # to keep gdb happy, disable optimisations
-            compilerOptimisationOptions='-O0',
-        ),
+            children=[
+                      
+                Toolchain(
+                    id='toolchain.osx.x64.llvm.debug',
+                    name='OS X Apple x86_64 LLVM Debug',
+                    description='The Apple OS X x86_64 LLVM toolchain for debug builds',
+                    
+                    category='debug',
+                    
+                    # to keep gdb happy, disable optimisations
+                    compilerOptimisationOptions='-O0',
+                ),
+        
+                Toolchain(
+                    id='toolchain.osx.x64.llvm.release',
+                    name='OS X Apple x86_64 LLVM Release',
+                    description='The Apple OS X x86_64 LLVM toolchain for release builds',
+                    
+                    category='release',
+                    
+                    # use an aggressive optimisation level
+                    compilerOptimisationOptions='-O3',
+                ),              
+            ],
+        ), 
 
         Toolchain(
-            id='toolchain.osx.llvm.release',
-            name='OS X Apple x86_64 LLVM Release',
-            description='The Apple OS X x86_64 LLVM toolchain for release builds',
+                  
+            id='toolchain.osx.x32.llvm.base',
+            name='OS X Apple x86_32 LLVM Base',
+            description='The Apple OS X x86_32 LLVM toolchain base',
             
-            category='release',
+            category='base',
+
+            compilerArchitecture='-arch=i386',
             
-            # use an aggressive optimisation level
-            compilerOptimisationOptions='-O3',
-        ),              
+            children=[
+                      
+                Toolchain(
+                    id='toolchain.osx.x32.llvm.debug',
+                    name='OS X Apple x86_32 LLVM Debug',
+                    description='The Apple OS X x86_32 LLVM toolchain for debug builds',
+                    
+                    category='debug',
+                    
+                    # to keep gdb happy, disable optimisations
+                    compilerOptimisationOptions='-O0',
+                ),
+        
+                Toolchain(
+                    id='toolchain.osx.x32.llvm.release',
+                    name='OS X Apple x86_32 LLVM Release',
+                    description='The Apple OS X x86_32 LLVM toolchain for release builds',
+                    
+                    category='release',
+                    
+                    # use an aggressive optimisation level
+                    compilerOptimisationOptions='-O3',
+                ),              
+            ],
+        ), 
+
     ],
 )
 
 
 Toolchain(
+          
     id='toolchain.osx.mp.gcc.base',
     name='OS X MacPorts x86_64 GCC Base',
     description='The OS X MacPorts x86_64 GCC toolchain base',
@@ -108,6 +162,7 @@ Toolchain(
     compilerMiscOptions='-fsigned-char -fmessage-length=0 -c',
     compilerDebugOptions='-g3',
     compilerOptimisationOptions='-O0',
+    compilerArchitecture='-arch=x86_64',
     
     linkerMiscOptions='',
     
@@ -154,7 +209,7 @@ Toolchain(
             name='OS X Mac Ports x86_64 GCC 4.6 Release',
             description='The OS X MacPorts x86_64 GCC 4.6 toolchain for release builds',
             
-            category='debug',
+            category='release',
             
             programNameSuffix='-mp-4.6',
             
@@ -180,7 +235,7 @@ Toolchain(
             name='OS X Mac Ports x86_64 GCC 4.7.2 Release',
             description='The OS X MacPorts x86_64 GCC 4.7 toolchain for release builds',
             
-            category='debug',
+            category='release',
             
             programNameSuffix='-mp-4.7',
             
@@ -188,5 +243,220 @@ Toolchain(
             compilerOptimisationOptions='-O3',
         ),
     
+    ],
+)
+
+
+Toolchain(
+          
+    id='toolchain.linux.gcc.base',
+    name='Linux GCC Base',
+    description='The Linux GCC toolchain base',
+    
+    category='base',
+    parent='package.os.toolchains.base',
+    
+    platformSystem='Linux',
+
+    programNamePrefix='',
+    
+    compilerObjectsExtension='o',
+    makeObjectsVariable='OBJS',
+    
+    compilerDepsOptions='-MMD -MP',
+    compilerOutputOptions='-o "$@"',
+    compilerInputOptions='"$<"',
+    compilerWarningOptions='-Wall',
+    
+    compilerMiscOptions='-fsigned-char -fmessage-length=0 -c',
+    compilerDebugOptions='-g3',
+    compilerOptimisationOptions='-O0',
+    
+    linkerMiscOptions='',
+    
+    cc=Tool(
+        programName='$(PREFIX)gcc$(SUFFIX)',
+        description='Linux GNU GCC',
+        options='',
+    ),
+          
+    cpp=Tool(
+        programName='$(PREFIX)g++$(SUFFIX)',
+        description='Linux GNU G++',    
+        options='',
+    ),
+          
+    asm=Tool(
+        programName='$(PREFIX)gcc$(SUFFIX)',
+        description='Linux GNU GCC',
+        options='',
+    ),
+
+    ld=Tool(
+        programName='$(PREFIX)g++$(SUFFIX)',
+        description='Linux GNU G++',    
+    ),
+    
+    children=[
+              
+        Toolchain(
+            id='toolchain.linux.gcc.debug',
+            name='Linux GCC Debug',
+            description='The Linux GCC toolchain for debug builds',
+            
+            category='debug',
+            
+            # to keep gdb happy, disable optimisations
+            compilerOptimisationOptions='-O0',
+        ),
+
+        Toolchain(
+            id='toolchain.linux.gcc.release',
+            name='Linux GCC Release',
+            description='The Linux GCC toolchain for release builds',
+            
+            category='release',
+            
+            # use an aggressive optimisation level
+            compilerOptimisationOptions='-O3',
+        ),
+
+
+        Toolchain(
+                  
+            id='toolchain.linux.x64.gcc.base',
+            name='Linux x86_64 GCC Base',
+            description='The Linux x86_64 GCC toolchain base',
+            
+            category='base',
+
+            programNameSuffix='',
+            
+            compilerArchitecture='-arch=x86_64',
+                        
+            children=[
+                      
+                Toolchain(
+                    id='toolchain.linux.x64.gcc46.debug',
+                    name='Linux x86_64 GCC 4.6 Debug',
+                    description='The Linux x86_64 GCC 4.6 toolchain for debug builds',
+                    
+                    category='debug',
+        
+                    programNameSuffix='-4.6',
+                    
+                    # to keep gdb happy, disable optimisations
+                    compilerOptimisationOptions='-O0',
+                ),
+        
+                Toolchain(
+                    id='toolchain.linux.x64.gcc46.release',
+                    name='Linux x86_64 GCC 4.6 Release',
+                    description='The Linux x86_64 GCC 4.6 toolchain for release builds',
+                    
+                    category='release',
+                    
+                    programNameSuffix='-4.6',
+        
+                    # use an aggressive optimisation level
+                    compilerOptimisationOptions='-O3',
+                ),
+        
+                Toolchain(
+                    id='toolchain.linux.x64.gcc47.debug',
+                    name='Linux x86_64 GCC 4.7 Debug',
+                    description='The Linux x86_64 GCC 4.7 toolchain for debug builds',
+                    
+                    category='debug',
+        
+                    programNameSuffix='-4.7',
+                    
+                    # to keep gdb happy, disable optimisations
+                    compilerOptimisationOptions='-O0',
+                ),
+        
+                Toolchain(
+                    id='toolchain.linux.x64.gcc47.release',
+                    name='Linux x86_64 GCC 4.7 Release',
+                    description='The Linux x86_64 GCC 4.7 toolchain for release builds',
+                    
+                    category='release',
+                    
+                    programNameSuffix='-4.7',
+        
+                    # use an aggressive optimisation level
+                    compilerOptimisationOptions='-O3',
+                ),
+            ],
+        ),
+              
+              
+        Toolchain(
+                   
+            id='toolchain.linux.x32.gcc.base',
+            name='Linux x86_32 GCC Base',
+            description='The Linux x86_32 GCC toolchain base',
+            
+            category='base',
+
+            programNameSuffix='',
+            
+            compilerArchitecture='-arch=i386',
+            
+            children=[
+                      
+                Toolchain(
+                    id='toolchain.linux.x32.gcc46.debug',
+                    name='Linux x86_32 GCC 4.6 Debug',
+                    description='The Linux x86_32 GCC 4.6 toolchain for debug builds',
+                    
+                    category='debug',
+        
+                    programNameSuffix='-4.6',
+                    
+                    # to keep gdb happy, disable optimisations
+                    compilerOptimisationOptions='-O0',
+                ),
+        
+                Toolchain(
+                    id='toolchain.linux.x32.gcc46.release',
+                    name='Linux x86_32 GCC 4.6 Release',
+                    description='The Linux x86_32 GCC 4.6 toolchain for release builds',
+                    
+                    category='release',
+                    
+                    programNameSuffix='-4.6',
+        
+                    # use an aggressive optimisation level
+                    compilerOptimisationOptions='-O3',
+                ),
+        
+                Toolchain(
+                    id='toolchain.linux.x32.gcc47.debug',
+                    name='Linux x86_32 GCC 4.7 Debug',
+                    description='The Linux x86_32 GCC 4.7 toolchain for debug builds',
+                    
+                    category='debug',
+        
+                    programNameSuffix='-4.7',
+                    
+                    # to keep gdb happy, disable optimisations
+                    compilerOptimisationOptions='-O0',
+                ),
+        
+                Toolchain(
+                    id='toolchain.linux.x32.gcc47.release',
+                    name='Linux x86_32 GCC 4.7 Release',
+                    description='The Linux x86_32 GCC 4.7 toolchain for release builds',
+                    
+                    category='release',
+                    
+                    programNameSuffix='-4.7',
+        
+                    # use an aggressive optimisation level
+                    compilerOptimisationOptions='-O3',
+                ),
+            ],
+        ),
     ],
 )
