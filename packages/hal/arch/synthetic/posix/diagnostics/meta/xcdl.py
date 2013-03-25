274de1d0cc25818920c71fa430bbc55e1fe787d4
Liviu Ionescu <ilg@livius.net>
2013-03-25 22:11:19 +0200

    POSIX diagnostics implementation

---
+++ packages/hal/arch/synthetic/posix/diagnostics/meta/xcdl.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+
+# This file is part of the µOS++ SE distribution.
+# Copyright (c) 2013 Liviu Ionescu.
+
+Component(
+    id='component.os.hal.arch.synthetic.posix.diagnostics',
+    name='Synthetic POSIX diagnostics implementation',
+    description='Implementation for the diagnostics classes.',
+            
+    children=[
+              
+        Component(
+            id='component.os.hal.arch.synthetic.posix.diagnostics.trace',
+            name='Synthetic POSIX trace implementation',
+            description='Implementation to output data from the trace class.',
+            
+            headerDefinition='OS_INCLUDE_HAL_ARCH_SYNTHETIC_POSIX_DIAGNOSTICS_TRACEIMPLEMENTATION',
+        
+            implements=[
+                'interface.os.portable.diagnostics.trace',
+            ],
+                  
+            sourceFiles=[
+                'TraceImplementation.cpp',
+            ],
+            
+        ),
+    ],
+)
