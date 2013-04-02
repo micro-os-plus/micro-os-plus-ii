8b3fee6256ae55b458fa743b93acf8ea478abb74
Liviu Ionescu <ilg@livius.net>
2013-04-03 01:02:42 +0300

    stm32-h103 xcdl defs added; not functional yet

---
+++ packages/hal/platform/olimex/stm32-h103/meta/xcdl.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+
+# This file is part of the µOS++ SE distribution.
+# Copyright (c) 2013 Liviu Ionescu.
+
+Package(
+    id='package.os.hal.platform.olimex.stm32h103',
+    name='Olimex STM32-H103 board',
+    description='Support for the Olimex STM32-H103 development board, \
+based on STM32F103.',
+    
+    category='board',
+    
+    # platform packages shall load corresponding hal architecture packages
+    loadPackages=[
+        'package.os.hal.architecture.cortexm.stm32f',
+    ],
+        
+    requirements=[
+        'enable("package.os.hal.architecture.cortexm.stm32f")',
+    ],
+        
+    implements=[
+        'interface.os.platform',
+    ],
+
+)
