9978ffe480a382a06540888ee9b8510b1ac75798
Liviu Ionescu <ilg@livius.net>
2013-04-04 00:38:21 +0300

    --dest and --full params accepted

--- scripts/runTests.sh
+++ scripts/runTests.sh
@@ -7,15 +7,28 @@ XCDLBUILD="xcdl-python.git/scripts/xcdlBuild.sh"
 REPO="micro-os-plus-se.git/packages"
 UNAME=`uname`
 
+FULL=""
 DEST=build
 
 declare -a testNames=( 'minimal' 'trace' 'fpos' 'ios_base' 'basic_ios' 'streambuf' 'ostream' 'ostreamconv' )
 
-# Check for custom destination folder
-if [ $# -gt 0 ]
-then
-  DEST=$1
-fi
+while [ $# -gt 0 ]
+do
+	#echo $1
+	if [ $1 == "--full" ]
+	then
+		#echo "Full"
+		FULL="Y"
+	elif [ $1 == "--dest" -a $# -gt 1 ]
+	then
+		#echo "Dest" $2
+		DEST=$2
+		shift
+	else
+		echo "unsupported" $1
+	fi
+	shift
+done
 
 # this is x86_64 on 64 bit machines
 MACHINE=$(uname -m)
@@ -62,11 +75,27 @@ if [ $UNAME == "Linux" ]
 then
 
 	time (
-		if [ "$MACHINE" == "x86_64" ]
+	
+		if [ -z "$FULL" ]
 		then
-			(PATH=$PATH; runTestArray "linux" "x64_gcc" "run")
+
+			# decide on 64/32 bits			
+			if [ "$MACHINE" == "x86_64" ]
+			then
+				(PATH=$PATH; runTestArray "linux" "x64_gcc" "run")
+			else
+				(PATH=$PATH; runTestArray "linux" "x32_gcc" "run")
+			fi
+
+		else
+			
+			if [ "$MACHINE" == "x86_64" ]
+			then
+				(PATH=$PATH; runTestArray "linux" "x64_gcc" "run")
+			fi
+			(PATH=$PATH; runTestArray "linux" "x32_gcc" "run")
+		
 		fi
-		(PATH=$PATH; runTestArray "linux" "x32_gcc" "run")
 	)
 
 elif [ $UNAME == "Darwin" ]
@@ -74,34 +103,49 @@ then
 
 	
 	time ( 
+		
+		if [ -z "$FULL" ]
+		then
+		
+			# clang is always available on OS X
+			# decide only on 64/32 bits			
+			if [ "$MACHINE" == "x86_64" ]
+			then
+				(PATH=$PATH; runTestArray "osx" "x64_llvm" "run")
+			else
+				(PATH=$PATH; runTestArray "osx" "x32_llvm" "run")
+			fi
+		
+		else
+		
+			# clang is always available on OS X
+			if [ "$MACHINE" == "x86_64" ]
+			then
+				(PATH=$PATH; runTestArray "osx" "x64_llvm" "run")
+			fi
+			(PATH=$PATH; runTestArray "osx" "x32_llvm" "run")
 	
-		# clang is always available on OS X
-		if [ "$MACHINE" == "x86_64" ]
-		then
-			(PATH=$PATH; runTestArray "osx" "x64_llvm" "run")
-		fi
-		(PATH=$PATH; runTestArray "osx" "x32_llvm" "run")
-
-		PATH_GCC47=/opt/local/bin
-		
-		if [ -x /opt/local/bin/g++-mp-4.7 ]
-		then
-			if [ "$MACHINE" == "x86_64" ]
+			PATH_GCC47=/opt/local/bin
+			
+			if [ -x /opt/local/bin/g++-mp-4.7 ]
 			then
-				(PATH=$PATH_GCC47:$PATH; runTestArray "osx" "x64_gcc47" "run")
+				if [ "$MACHINE" == "x86_64" ]
+				then
+					(PATH=$PATH_GCC47:$PATH; runTestArray "osx" "x64_gcc47" "run")
+				fi
+				(PATH=$PATH_GCC47:$PATH; runTestArray "osx" "x32_gcc47" "run")
 			fi
-			(PATH=$PATH_GCC47:$PATH; runTestArray "osx" "x32_gcc47" "run")
-		fi
-		
-		PATH_GCC46=/opt/local/bin
-		
-		if [ -x /opt/local/bin/g++-mp-4.6 ]
-		then
-			if [ "$MACHINE" == "x86_64" ]
+			
+			PATH_GCC46=/opt/local/bin
+			
+			if [ -x /opt/local/bin/g++-mp-4.6 ]
 			then
-				(PATH=$PATH_GCC46:$PATH; runTestArray "osx" "x64_gcc46" "run")
+				if [ "$MACHINE" == "x86_64" ]
+				then
+					(PATH=$PATH_GCC46:$PATH; runTestArray "osx" "x64_gcc46" "run")
+				fi
+				(PATH=$PATH_GCC46:$PATH; runTestArray "osx" "x32_gcc46" "run")
 			fi
-			(PATH=$PATH_GCC46:$PATH; runTestArray "osx" "x32_gcc46" "run")
 		fi
 	)
 
@@ -112,20 +156,34 @@ echo "XCDL runTests: completed, results in $DEST"
 echo
 echo "PWD=`pwd`"
 
-if [ "$MACHINE" == "x86_64" ]
+
+if [ -z "$FULL" ]
 then
-	echo "64 bit versions included"
+
+	if [ "$MACHINE" == "x86_64" ]
+	then
+		echo "64 bits test only"
+	else
+		echo "32 bits test only"
+	fi
+
 else
-	echo "32 bit versions only"
-fi
 
-if [ -x /opt/local/bin/g++-mp-4.7 ]
-then
-	echo "g++-mp-4.7 included"
-fi
+	if [ "$MACHINE" == "x86_64" ]
+	then
+		echo "both 64/32 bits tests included"
+	else
+		echo "32 bits tests only"
+	fi
 
-if [ -x /opt/local/bin/g++-mp-4.6 ]
-then
-	echo "g++-mp-4.6 included"
-fi
+	if [ -x /opt/local/bin/g++-mp-4.7 ]
+	then
+		echo "g++-mp-4.7 tests included"
+	fi
+	
+	if [ -x /opt/local/bin/g++-mp-4.6 ]
+	then
+		echo "g++-mp-4.6 tests included"
+	fi
 
+fi
