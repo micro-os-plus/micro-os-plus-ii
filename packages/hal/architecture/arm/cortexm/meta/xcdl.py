081824a8d842a66ffb4abd7f1ea264034597c80e
Liviu Ionescu <ilg@livius.net>
2013-04-08 21:49:42 +0300

    Cortex M0, M0+, M3, M4 architectures defined

--- packages/hal/architecture/arm/cortexm/meta/xcdl.py
+++ packages/hal/architecture/arm/cortexm/meta/xcdl.py
@@ -5,10 +5,8 @@
 
 Package(
     id='package.os.hal.architecture.arm.cortexm',
-    name='ARM Cortex-M Architecture',
+    name='ARM Cortex-M Architectures',
     description='Support for ARM Cortex-M processors.',
-    
-    category='architecture',
     
     isEnabled=False,
     
@@ -16,11 +14,74 @@ Package(
         'ArchitectureImplementation.cpp',
     ],
     
-    implements=[
-        # mark this is an architecture
-        'interface.os.architecture',
+    copyFiles=[
+        ('../ldscripts/sections.ld', 'ldscripts/'),
     ],
 
-    headerDefinition='OS_INCLUDE_HAL_ARCHITECTURE_ARM_CORTEXM',
-          
+    headerDefinition='OS_INCLUDE_HAL_ARCHITECTURE_ARM_CORTEX_M',
+    
+    children=[
+              
+        Component(
+            id='package.os.hal.architecture.arm.cortexm0',
+            name='ARM Cortex-M0 Architecture',
+            description='Support for ARM Cortex-M0 processors.',
+
+            isEnabled=False,
+            implements=[
+                # mark this is an architecture
+                'interface.os.architecture',
+            ],
+
+            headerDefinition='OS_INCLUDE_HAL_ARCHITECTURE_ARM_CORTEX_M0',
+            
+        ),
+
+        Component(
+            id='package.os.hal.architecture.arm.cortexm0p',
+            name='ARM Cortex-M0+ Architecture',
+            description='Support for ARM Cortex-M0+ processors.',
+
+            isEnabled=False,
+            implements=[
+                # mark this is an architecture
+                'interface.os.architecture',
+            ],
+
+            headerDefinition='OS_INCLUDE_HAL_ARCHITECTURE_ARM_CORTEX_M0P',
+            
+        ),
+
+        Component(
+            id='package.os.hal.architecture.arm.cortexm3',
+            name='ARM Cortex-M3 Architecture',
+            description='Support for ARM Cortex-M3 processors.',
+
+            isEnabled=False,
+            implements=[
+                # mark this is an architecture
+                'interface.os.architecture',
+            ],
+
+            headerDefinition='OS_INCLUDE_HAL_ARCHITECTURE_ARM_CORTEX_M3',
+            
+        ),
+
+        Component(
+            id='package.os.hal.architecture.arm.cortexm4',
+            name='ARM Cortex-M4 Architecture',
+            description='Support for ARM Cortex-M4 processors.',
+
+            isEnabled=False,
+            implements=[
+                # mark this is an architecture
+                'interface.os.architecture',
+            ],
+
+            headerDefinition='OS_INCLUDE_HAL_ARCHITECTURE_ARM_CORTEX_M4',
+            
+        ),
+
+
+    ],     
 )
