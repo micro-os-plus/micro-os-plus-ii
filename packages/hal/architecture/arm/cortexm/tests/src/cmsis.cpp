02dfcdb66cd8633211c17841779b15f8850a2c0c
Liviu Ionescu <ilg@livius.net>
2013-04-11 00:50:48 +0300

    compiles with InterruptNumbers, but only if included before cmsis

--- packages/hal/architecture/arm/cortexm/tests/src/cmsis.cpp
+++ packages/hal/architecture/arm/cortexm/tests/src/cmsis.cpp
@@ -5,9 +5,12 @@
 
 #include "portable/core/include/OS.h"
 
+#include "hal/architecture/arm/cortexm/stm32f/include/InterruptNumbers.h"
+
 #if defined(OS_INCLUDE_HAL_MCU_DEVICE_STM32F103RB)
 #include "hal/architecture/arm/cortexm/stm32f/lib/cmsis/stm32f10x/include/stm32f10x.h"
 #endif
+
 
 int
 main()
