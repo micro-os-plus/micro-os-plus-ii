925f1aa992f9046ed64ba784d02df6d02d5ec2d0
Liviu Ionescu <ilg@livius.net>
2013-04-14 01:06:56 +0300

    renamed as selector

--- packages/portable/infrastructure/include/TestSuiteImplementationSelector.h
+++ packages/portable/infrastructure/include/TestSuiteImplementationSelector.h
@@ -3,6 +3,9 @@
 // Copyright (c) 2013 Liviu Ionescu.
 //
 
+/// \file
+/// \brief Selector of one TestSuite implementation.
+
 #ifndef OS_PORTABLE_INFRASTRUCTURE_TESTSUITEIMPLEMENTATION_H_
 #define OS_PORTABLE_INFRASTRUCTURE_TESTSUITEIMPLEMENTATION_H_
 
