d1e06eee21bd359e226961d1539d373025c4851f
Liviu Ionescu <ilg@livius.net>
2013-05-02 22:29:28 +0300

    family typedefs

---
+++ packages/hal/architecture/arm/cortexm/stm32f/stm32f4/include/FamilyDefinitions.h
@@ -0,0 +1,37 @@
+//
+// This file is part of the µOS++ SE distribution.
+// Copyright (c) 2013 Liviu Ionescu.
+//
+
+/// \file
+/// \brief STM32F4 definitions.
+
+#ifndef HAL_ARCHITECTURE_ARM_CORTEXM_STM32F_STM32F4_FAMILYIDEFINITIONS_H_
+#define HAL_ARCHITECTURE_ARM_CORTEXM_STM32F_STM32F4_FAMILYIDEFINITIONS_H_
+
+#include "portable/core/include/ConfigDefines.h"
+
+#if defined(OS_INCLUDE_HAL_MCU_FAMILY_STM32F4) || defined(__DOXYGEN__)
+
+namespace hal
+{
+  namespace stm32f4
+  {
+    // ------------------------------------------------------------------------
+
+    // Register types
+    typedef uint32_t volatile regReadWrite_t;
+    typedef uint32_t const volatile regReadOnly_t;
+    typedef uint32_t volatile regWriteOnly_t;
+    typedef uint32_t regNotAllocated_t;
+
+    typedef uint32_t address_t;
+    typedef uint32_t reg32_t;
+    typedef uint16_t reg16_t;
+
+  // --------------------------------------------------------------------------
+
+  }// namespace stm32f4
+} // namespace hal
+#endif // defined(OS_INCLUDE_HAL_MCU_FAMILY_STM32F4)
+#endif // HAL_ARCHITECTURE_ARM_CORTEXM_STM32F_STM32F4_FAMILYIDEFINITIONS_H_
