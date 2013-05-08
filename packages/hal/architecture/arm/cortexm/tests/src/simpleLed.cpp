19b5fe85a6e9d1d975409d0c1ee90fc43d56cf80
Liviu Ionescu <ilg@livius.net>
2013-05-08 16:27:16 +0300

    GPIO_PuPd_NOPULL added for F4

--- packages/hal/architecture/arm/cortexm/tests/src/simpleLed.cpp
+++ packages/hal/architecture/arm/cortexm/tests/src/simpleLed.cpp
@@ -91,6 +91,7 @@ Led::powerUp(void)
     GPIO_InitStructure.GPIO_Mode = GPIO_Mode_OUT;
     GPIO_InitStructure.GPIO_OType = GPIO_OType_PP;
     GPIO_InitStructure.GPIO_Speed = GPIO_Speed_100MHz;
+    GPIO_InitStructure.GPIO_PuPd = GPIO_PuPd_NOPULL;
     GPIO_Init(LED_PORT_POINTER, &GPIO_InitStructure);
   }
 
