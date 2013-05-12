8d5bb47e8e7d7103e86066b057404aa0967221be
Liviu Ionescu <ilg@livius.net>
2013-05-12 23:26:25 +0300

    simple IWDG implementation; configuration functions not yet implemented

---
+++ packages/hal/architecture/arm/cortexm/stm32f/stm32f4/peripheral/include/Iwdg.h
@@ -0,0 +1,301 @@
+//
+// This file is part of the µOS++ SE distribution.
+// Copyright (c) 2013 Liviu Ionescu.
+//
+
+/// \file
+/// \brief STM32F4 Independent Watchdog high level classes.
+///
+
+#ifndef HAL_ARCHITECTURE_ARM_CORTEXM_STM32F_STM32F4_PERIPHERAL_IWDG_H_
+#define HAL_ARCHITECTURE_ARM_CORTEXM_STM32F_STM32F4_PERIPHERAL_IWDG_H_
+
+#include "portable/core/include/ConfigDefines.h"
+
+#if defined(OS_INCLUDE_HAL_MCU_FAMILY_STM32F4) || defined(__DOXYGEN__)
+
+#include "hal/architecture/arm/cortexm/include/ArchitectureDefinitions.h"
+#include "hal/architecture/arm/cortexm/stm32f/stm32f4/peripheral/include/IwdgHardware.h"
+
+//#include "hal/architecture/arm/cortexm/peripheral/include/BitBanding.h"
+
+namespace hal
+{
+  namespace stm32f4
+  {
+    // ========================================================================
+#if 0
+    /// \headerfile Iwdg.h "hal/architecture/arm/cortexm/stm32f/stm32f4/peripheral/include/Iwdg.h"
+    /// \ingroup stm32f4_iwdg
+    /// \nosubgrouping
+    ///
+    /// \brief IWDG class (static version).
+    ///
+    /// \details
+    /// This class template provides full control of a STM32F4 GPIO pin.
+    /// It is intended for most common cases when both the port number
+    /// and the bit number are constants known at compile time.
+    ///
+    /// For performance reasons, all member functions are inline and very
+    /// simple, performing just the required memory access(es).
+    ///
+    /// Using it is typical for static classes, although a default
+    /// constructor is provided, so you can instantiate it as well.
+    ///
+    /// \code{.cpp}
+    /// template class hal::stm32f4::TGpioPin<XPORT, XBIT>;
+    /// typedef class hal::stm32f4::TGpioPin<XPORT, XBIT> XGPIO;
+    ///
+    /// XGPIO::setPinHigh();
+    /// \endcode
+    class Iwdg
+      {
+      public:
+        /// \name Constructors/destructor
+        /// @{
+
+        /// \brief Default constructor.
+        Iwdg() = default;
+
+        /// @} end of name Constructors/destructor
+
+        // ----- Getters --------------------------------------------------------
+        /// \name Getters
+        /// @{
+
+        /// \brief Get a reference to the port registers.
+        ///
+        /// \par Parameters
+        ///    None.
+        /// \return A reference to the port registers.
+        static iwdg::Registers&
+        getRegisters(void);
+
+        /// @} end of name Getters
+
+        // ----- Configuration functions ----------------------------------------
+        /// \name Configuration functions
+        /// @{
+
+        /// @} end of name Configuration functions
+
+        /// \name Member functions
+        /// @{
+
+        /// \brief Start the watchdog.
+        ///
+        /// \par Parameters
+        ///    None.
+        /// \par Returns
+        ///    Nothing.
+        static void
+        start(void);
+
+        /// \brief Reload the watchdog.
+        ///
+        /// \par Parameters
+        ///    None.
+        /// \par Returns
+        ///    Nothing.
+        static void
+        reload(void);
+
+        /// @} end of name Member functions
+
+        // ----------------------------------------------------------------------
+
+      private:
+        /// \name Private members
+        /// @{
+
+        /// \brief The address of the device hardware registers.
+        ///
+        /// \details
+        /// This value is the device base address.
+        /// For convenient access, it is represented as a reference.
+        static constexpr iwdg::Registers& m_registers =
+        *reinterpret_cast<iwdg::Registers*>(0);//(iwdg::Registers::MEMORY_BASE_ADDRESS);
+
+        /// @} end of name Private members
+      };
+
+    // ----- Inline member function definitions -------------------------------
+
+    inline iwdg::Registers&
+    __attribute__((always_inline))
+    Iwdg::getRegisters(void)
+      {
+        return m_registers;
+      }
+
+    inline void
+    __attribute__((always_inline))
+    Iwdg::start(void)
+      {
+        m_registers.writeKey(iwdg::START_KEY);
+      }
+
+    inline void
+    __attribute__((always_inline))
+    Iwdg::reload(void)
+      {
+        m_registers.writeKey(iwdg::RELOAD_KEY);
+      }
+#else
+    /// \headerfile Iwdg.h "hal/architecture/arm/cortexm/stm32f/stm32f4/peripheral/include/Iwdg.h"
+    /// \ingroup stm32f4_iwdg
+    /// \nosubgrouping
+    ///
+    /// \brief Independent Watchdog class template (static version).
+    ///
+    /// \details
+    /// This class template provides control of the STM32F4 IWDG. You do not
+    /// have to use it directly, the `hal::stm32f4::Iwdg` instantiation
+    /// is available.
+    ///
+    /// For performance reasons, all member functions are inline and very
+    /// simple, performing just the required memory access(es).
+    ///
+    /// Using it is typical for static classes, although a default
+    /// constructor is provided, so you can instantiate it as well.
+    ///
+    /// \par Example
+    /// \code{.cpp}
+    /// hal::stm32f4::Iwdg::reload();
+    /// \endcode
+    template<hal::cortexm::address_t A>
+      class TIwdg
+      {
+      public:
+        /// \name Constructors/destructor
+        /// @{
+
+        /// \brief Default constructor.
+        TIwdg() = default;
+
+        /// @} end of name Constructors/destructor
+
+        // ----- Getters --------------------------------------------------------
+        /// \name Getters
+        /// @{
+
+        /// \brief Get a reference to the peripheral registers.
+        ///
+        /// \par Parameters
+        ///    None.
+        /// \return A reference to the peripheral registers.
+        static iwdg::Registers&
+        getRegisters(void);
+
+        /// @} end of name Getters
+
+        // ----- Configuration functions ----------------------------------------
+        /// \name Configuration functions
+        /// @{
+
+        static void
+        configurePrescaler(iwdg::PrescalerDivider);
+
+        static iwdg::PrescalerDivider
+        retrievePrescaler(void);
+
+        static void
+        configureReload(iwdg::reloadValue_t value);
+
+        static iwdg::reloadValue_t
+        retrieveReload(void);
+
+        /// @} end of name Configuration functions
+
+        /// \name Member functions
+        /// @{
+
+        /// \brief Start the watchdog downcounter.
+        ///
+        /// \par Parameters
+        ///    None.
+        /// \par Returns
+        ///    Nothing.
+        static void
+        start(void);
+
+        /// \brief Reload the watchdog downcounter.
+        ///
+        /// \par Parameters
+        ///    None.
+        /// \par Returns
+        ///    Nothing.
+        static void
+        reload(void);
+
+        /// @} end of name Member functions
+
+        // ----------------------------------------------------------------------
+
+      private:
+        /// \name Private members
+        /// @{
+
+        /// \brief The address of the peripheral hardware registers.
+        ///
+        /// \details
+        /// This value is the peripheral base address.
+        /// For convenient access, it is represented as a reference.
+        static constexpr iwdg::Registers& m_registers =
+            *reinterpret_cast<iwdg::Registers*>(A);
+
+        /// @} end of name Private members
+      };
+
+    // ----- Inline member function definitions -------------------------------
+
+    template<hal::cortexm::address_t A>
+      inline iwdg::Registers&
+      __attribute__((always_inline))
+      TIwdg<A>::getRegisters(void)
+      {
+        return m_registers;
+      }
+
+    /// \details
+    /// When the independent watchdog is started by writing the value
+    /// 0xCCCC in the Key register (IWDG_KR), the counter starts
+    /// counting down from the reset value of 0xFFF. When it reaches
+    /// the end of count value (0x000) a reset signal is generated
+    /// (IWDG reset).
+    template<hal::cortexm::address_t A>
+      inline void
+      __attribute__((always_inline))
+      TIwdg<A>::start(void)
+      {
+        m_registers.writeKey(iwdg::START_KEY);
+      }
+
+    /// \details
+    /// Whenever the key value 0xAAAA is written in the IWDG_KR register,
+    /// the IWDG_RLR value is reloaded in the counter and the watchdog
+    /// reset is prevented.
+    template<hal::cortexm::address_t A>
+      inline void
+      __attribute__((always_inline))
+      TIwdg<A>::reload(void)
+      {
+        m_registers.writeKey(iwdg::RELOAD_KEY);
+      }
+
+    // ------------------------------------------------------------------------
+
+    /// \ingroup stm32f4_iwdg
+    /// \brief IWDG class based on the template.
+    ///
+    /// \details
+    /// Use this static class to access the IWDG peripheral.
+    typedef TIwdg<iwdg::Registers::MEMORY_BASE_ADDRESS> Iwdg;
+
+#endif
+  // ==========================================================================
+  }// namespace stm32f4
+} // namespace os
+
+#endif // defined(OS_INCLUDE_HAL_MCU_FAMILY_STM32F4)
+#endif // HAL_ARCHITECTURE_ARM_CORTEXM_STM32F_STM32F4_PERIPHERAL_IWDG_H_
---
+++ packages/hal/architecture/arm/cortexm/stm32f/stm32f4/peripheral/include/IwdgHardware.h
@@ -0,0 +1,197 @@
+//
+// This file is part of the µOS++ SE distribution.
+// Copyright (c) 2013 Liviu Ionescu.
+//
+
+/// \file
+/// \brief STM32F4 Independent Watchdog memory mapped registers & definitions.
+
+#ifndef HAL_ARCHITECTURE_ARM_CORTEXM_STM32F_STM32F4_PERIPHERAL_IWDGHARDWARE_H_
+#define HAL_ARCHITECTURE_ARM_CORTEXM_STM32F_STM32F4_PERIPHERAL_IWDGHARDWARE_H_
+
+#include "portable/core/include/ConfigDefines.h"
+
+#if defined(OS_INCLUDE_HAL_MCU_FAMILY_STM32F4) || defined(__DOXYGEN__)
+
+#include "hal/architecture/arm/cortexm/include/ArchitectureDefinitions.h"
+#include "hal/architecture/arm/cortexm/peripheral/include/BitBanding.h"
+
+#include "hal/architecture/arm/cortexm/stm32f/stm32f4/include/PeripheralMemoryMap.h"
+
+namespace hal
+{
+  namespace stm32f4
+  {
+    namespace iwdg
+    {
+
+      typedef hal::cortexm::portNumber_t portNumber_t;
+      typedef hal::cortexm::bitNumber_t bitNumber_t;
+      typedef hal::cortexm::index_t index_t;
+
+      typedef hal::cortexm::bitband::bitValue_t bitValue_t;
+      typedef hal::cortexm::reg16_t reg16_t;
+      typedef hal::cortexm::reg32_t reg32_t;
+    }
+
+    namespace iwdg
+    {
+      /// \addtogroup stm32f4_iwdg
+      /// @{
+
+      // ----------------------------------------------------------------------
+
+      static const reg16_t ACCESS_KEY = 0x5555U;
+      static const reg16_t START_KEY = 0xCCCCU;
+      static const reg16_t RELOAD_KEY = 0xAAAAU;
+
+      // ----------------------------------------------------------------------
+
+      /// \brief Prescaler divider type.
+      typedef uint32_t prescalerDivider_t;
+
+      /// \brief Prescaler divider values.
+      enum class PrescalerDivider
+        : prescalerDivider_t
+          {
+            _4 = 0, _8, _16, _32, _64, _128, _256, _256b
+      };
+
+      // ----------------------------------------------------------------------
+
+        /// \brief Reload register type.
+      typedef uint16_t reloadValue_t;
+
+      // ----------------------------------------------------------------------
+
+      /// @} end of addtogroup stm32f4_iwdg
+
+      // ======================================================================
+
+      /// \headerfile IwdgHardware.h "hal/architecture/arm/cortexm/stm32f/stm32f4/peripheral/include/IwdgHardware.h"
+      /// \ingroup stm32f4_iwdg
+      /// \nosubgrouping
+      ///
+      /// \brief IWDG memory mapped registers.
+      ///
+      /// \warning This class is packed and mapped over a specific memory
+      ///       area. DO NOT change the members order or add new data members.
+      class __attribute__((packed)) Registers
+      {
+      public:
+        // --------------------------------------------------------------------
+
+        /// \name Constructors/destructor
+        /// @{
+
+        /// \brief Deleted constructor
+        Registers() = delete;
+
+        /// @} end of name Constructors/destructor
+
+        /// \name Public member functions
+        /// @{
+
+        /// \brief Write the key register.
+        void
+        writeKey(const reg16_t value);
+
+        /// \brief Read the prescaler register.
+        reg16_t
+        readPrescaler(void);
+
+        /// \brief Write the prescaler register.
+        void
+        writePrescaler(const reg16_t value);
+
+        /// \brief Read the reload register.
+        reg16_t
+        readReload(void);
+
+        /// \brief Write the reload register.
+        void
+        writeReload(const reg16_t value);
+
+        /// \brief Read the status register.
+        reg16_t
+        readStatus(void);
+
+        /// @} end of Public member functions
+
+        /// \name Memory definitions
+        /// @{
+
+        /// \brief Base address of the device.
+        static const hal::cortexm::address_t MEMORY_BASE_ADDRESS =
+            PeripheralMemoryMap::IWDG_BASE_ADDRESS;
+
+        /// @} end of name Memory definitions
+
+      private:
+        /// \name Private memory mapped registers
+        /// @{
+
+        /// \brief 0x0000 - Key register.
+        hal::cortexm::regWriteOnly_t kr;
+        /// \brief 0x0004 - Prescaler register.
+        hal::cortexm::regReadWrite_t pr;
+        /// \brief 0x0008 - Reload register.
+        hal::cortexm::regReadWrite_t rlr;
+        /// \brief 0x000C - Status register.
+        hal::cortexm::regReadOnly_t sr;
+
+        /// @} end of name Memory mapped registers
+
+      };
+
+      // ----- Inline member function definitions -----------------------------
+
+      inline void
+      __attribute__((always_inline))
+      Registers::writeKey(const reg16_t value)
+      {
+        this->kr = value;
+      }
+
+      inline reg16_t
+      __attribute__((always_inline))
+      Registers::readPrescaler(void)
+      {
+        return this->pr;
+      }
+
+      inline void
+      __attribute__((always_inline))
+      Registers::writePrescaler(const reg16_t value)
+      {
+        this->pr = value;
+      }
+
+      inline reg16_t
+      __attribute__((always_inline))
+      Registers::readReload(void)
+      {
+        return this->rlr;
+      }
+
+      inline void
+      __attribute__((always_inline))
+      Registers::writeReload(const reg16_t value)
+      {
+        this->rlr = value;
+      }
+
+      inline reg16_t
+      __attribute__((always_inline))
+      Registers::readStatus(void)
+      {
+        return this->sr;
+      }
+
+    // ========================================================================
+    }// namespace iwdg
+  } // namespace stm32f4
+} // namespace os
+
+#endif // defined(OS_INCLUDE_HAL_MCU_FAMILY_STM32F4)
+#endif // HAL_ARCHITECTURE_ARM_CORTEXM_STM32F_STM32F4_PERIPHERAL_IWDGHARDWARE_H_
