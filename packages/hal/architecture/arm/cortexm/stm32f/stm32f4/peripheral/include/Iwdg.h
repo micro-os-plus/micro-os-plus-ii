fc33a16faee64d4300d80dc6c3d5d600dd9baad9
Liviu Ionescu <ilg@livius.net>
2013-05-13 19:33:32 +0300

    static class removed

--- packages/hal/architecture/arm/cortexm/stm32f/stm32f4/peripheral/include/Iwdg.h
+++ packages/hal/architecture/arm/cortexm/stm32f/stm32f4/peripheral/include/Iwdg.h
@@ -24,124 +24,6 @@ namespace hal
   namespace stm32f4
   {
     // ========================================================================
-#if 0
-    /// \headerfile Iwdg.h "hal/architecture/arm/cortexm/stm32f/stm32f4/peripheral/include/Iwdg.h"
-    /// \ingroup stm32f4_iwdg
-    /// \nosubgrouping
-    ///
-    /// \brief IWDG class (static version).
-    ///
-    /// \details
-    /// This class template provides full control of a STM32F4 GPIO pin.
-    /// It is intended for most common cases when both the port number
-    /// and the bit number are constants known at compile time.
-    ///
-    /// For performance reasons, all member functions are inline and very
-    /// simple, performing just the required memory access(es).
-    ///
-    /// Using it is typical for static classes, although a default
-    /// constructor is provided, so you can instantiate it as well.
-    ///
-    /// \code{.cpp}
-    /// template class hal::stm32f4::TGpioPin<XPORT, XBIT>;
-    /// typedef class hal::stm32f4::TGpioPin<XPORT, XBIT> XGPIO;
-    ///
-    /// XGPIO::setPinHigh();
-    /// \endcode
-    class Iwdg
-      {
-      public:
-        /// \name Constructors/destructor
-        /// @{
-
-        /// \brief Default constructor.
-        Iwdg() = default;
-
-        /// @} end of name Constructors/destructor
-
-        // ----- Getters --------------------------------------------------------
-        /// \name Getters
-        /// @{
-
-        /// \brief Get a reference to the port registers.
-        ///
-        /// \par Parameters
-        ///    None.
-        /// \return A reference to the port registers.
-        static iwdg::Registers&
-        getRegisters(void);
-
-        /// @} end of name Getters
-
-        // ----- Configuration functions ----------------------------------------
-        /// \name Configuration functions
-        /// @{
-
-        /// @} end of name Configuration functions
-
-        /// \name Member functions
-        /// @{
-
-        /// \brief Start the watchdog.
-        ///
-        /// \par Parameters
-        ///    None.
-        /// \par Returns
-        ///    Nothing.
-        static void
-        start(void);
-
-        /// \brief Reload the watchdog.
-        ///
-        /// \par Parameters
-        ///    None.
-        /// \par Returns
-        ///    Nothing.
-        static void
-        reload(void);
-
-        /// @} end of name Member functions
-
-        // ----------------------------------------------------------------------
-
-      private:
-        /// \name Private members
-        /// @{
-
-        /// \brief The address of the device hardware registers.
-        ///
-        /// \details
-        /// This value is the device base address.
-        /// For convenient access, it is represented as a reference.
-        static constexpr iwdg::Registers& m_registers =
-        *reinterpret_cast<iwdg::Registers*>(0);//(iwdg::Registers::MEMORY_BASE_ADDRESS);
-
-        /// @} end of name Private members
-      };
-
-    // ----- Inline member function definitions -------------------------------
-
-    inline iwdg::Registers&
-    __attribute__((always_inline))
-    Iwdg::getRegisters(void)
-      {
-        return m_registers;
-      }
-
-    inline void
-    __attribute__((always_inline))
-    Iwdg::start(void)
-      {
-        m_registers.writeKey(iwdg::START_KEY);
-      }
-
-    inline void
-    __attribute__((always_inline))
-    Iwdg::reload(void)
-      {
-        m_registers.writeKey(iwdg::RELOAD_KEY);
-      }
-#else
     /// \headerfile Iwdg.h "hal/architecture/arm/cortexm/stm32f/stm32f4/peripheral/include/Iwdg.h"
     /// \ingroup stm32f4_iwdg
     /// \nosubgrouping
@@ -292,7 +174,6 @@ namespace hal
     /// Use this static class to access the IWDG peripheral.
     typedef TIwdg<iwdg::Registers::MEMORY_BASE_ADDRESS> Iwdg;
 
-#endif
   // ==========================================================================
   }// namespace stm32f4
 } // namespace os
