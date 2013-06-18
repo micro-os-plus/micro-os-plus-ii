c8cc638361d2b4233141de6d81d94dfe32023d09
Liviu Ionescu <ilg@livius.net>
2013-06-18 20:04:07 +0300

    stack definitions added in hal::arch, for both 32 and 64 bit

---
+++ packages/hal/architecture/synthetic/posix/include/ArchitectureDefinitions.h
@@ -0,0 +1,48 @@
+//
+// This file is part of the µOS++ SE distribution.
+// Copyright (c) 2013 Liviu Ionescu.
+//
+
+/// \file
+/// \brief Cortex-M definitions.
+
+#ifndef HAL_ARCHITECTURE_SYNTHETIC_POSIX_ARCHITECTUREDEFINITIONS_H_
+#define HAL_ARCHITECTURE_SYNTHETIC_POSIX_ARCHITECTUREDEFINITIONS_H_
+
+#include "portable/core/include/ConfigDefines.h"
+
+#if defined(OS_INCLUDE_HAL_ARCHITECTURE_SYNTHETIC_POSIX) || defined(__DOXYGEN__)
+
+#include <stdint.h>
+
+namespace hal
+{
+  namespace posix
+  {
+  // TBD
+  }// namespace posix
+} // namespace hal
+
+// Portable definitions.
+namespace hal
+{
+  namespace arch
+  {
+#if defined(__x86_64__)
+    /// \brief Type definition of a stack word unit.
+    typedef uint64_t stackElement_t;
+
+    static const stackElement_t STACK_FILL = 0x55AA5A5AA5A5AA55;
+#else
+    /// \brief Type definition of a stack word unit.
+    typedef uint32_t stackElement_t;
+
+    static const stackElement_t STACK_FILL = 0x55AAAA55;
+#endif
+
+    typedef uint32_t stackSize_t;
+
+  } // namespace arch
+} // namespace hal
+#endif // defined(OS_INCLUDE_HAL_ARCHITECTURE_SYNTHETIC_POSIX)
+#endif // HAL_ARCHITECTURE_SYNTHETIC_POSIX_ARCHITECTUREDEFINITIONS_H_
