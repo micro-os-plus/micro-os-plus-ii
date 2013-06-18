f467b5d8b21fda7bfc73245618fb85b63d00d1f1
Liviu Ionescu <ilg@livius.net>
2013-06-18 20:05:33 +0300

    context declarations added, no functionality defined yet

--- packages/hal/architecture/synthetic/posix/include/ArchitectureImplementation.h
+++ packages/hal/architecture/synthetic/posix/include/ArchitectureImplementation.h
@@ -15,6 +15,9 @@
 
 #include "portable/core/include/ArchitectureBase.h"
 
+// Include architecture definitions, like various types.
+#include "hal/architecture/synthetic/posix/include/ArchitectureDefinitions.h"
+
 namespace hal
 {
   namespace posix
@@ -26,9 +29,23 @@ namespace hal
     /// \nosubgrouping
     ///
     /// \brief Generic POSIX architecture implementation class.
+    ///
+    /// \details
+    /// This class provides generic POSIX architecture support.
     class ArchitectureImplementation : public os::core::ArchitectureBase
     {
     public:
+
+      /// \name Constructors/destructor
+      /// @{
+
+      /// \brief Deleted constructor.
+      ArchitectureImplementation() = default;
+
+      /// @} end of name Constructors/destructor
+
+      /// \name Public member functions
+      /// @{
 
 #if defined(DEBUG)
       /// \brief Put architecture greeting.
@@ -40,7 +57,86 @@ namespace hal
       static void
       putGreeting(void);
 #endif
+
+      /// @} end of name Public member functions
+
+      /// \headerfile ArchitectureImplementation.h "hal/architecture/synthetic/posix/include/ArchitectureImplementation.h"
+      /// \ingroup posix
+      /// \nosubgrouping
+      ///
+      /// \brief POSIX context handling.
+      ///
+      /// \details
+      /// This class provides generic POSIX context handling support.
+      class Context
+      {
+      public:
+        /// \name Constructors/destructor
+        /// @{
+
+        /// \brief Default constructor.
+        Context() = default;
+
+        /// @} end of name Constructors/destructor
+
+        /// \name Public member functions
+        /// @{
+
+        /// \brief Create the initial context in the stack object.
+        ///
+        /// \param [in] pStackTop Pointer to the last stack element.
+        /// \param [in] entryPoint Pointer to thread code.
+        /// \param [in] pParameters Pointer to thread parameters.
+        /// \par Returns
+        ///    Nothing.
+        static hal::arch::stackElement_t*
+        createInitial(hal::arch::stackElement_t* pStackTop,
+            os::core::threadEntryPoint_t entryPoint, void* pParameters);
+
+        /// \brief Save the current context on the stack.
+        ///
+        /// \par Parameters
+        ///    None.
+        /// \par Returns
+        ///    Nothing.
+        static void
+        save(void);
+
+        /// \brief Restore the current context from the stack.
+        ///
+        /// \par Parameters
+        ///    None.
+        /// \par Returns
+        ///    Nothing.
+        static void
+        restore(void);
+
+        /// @} end of name Public member functions
+
+      };
+
+      Context context;
+
     };
+
+    /// \details
+     /// TBD
+     inline void
+     __attribute__((always_inline))
+     ArchitectureImplementation::Context::save(void)
+     {
+       // TBD
+     }
+
+     /// \details
+     /// TBD
+     inline void
+     __attribute__((always_inline))
+     ArchitectureImplementation::Context::restore(void)
+     {
+       // TBD
+     }
+
   } // namespace posix
 
   // ------------------------------------------------------------------------
