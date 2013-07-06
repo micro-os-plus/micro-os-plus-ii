ab90cfe5a0938d3aa382eb8ead500d5460746a13
Liviu Ionescu <ilg@livius.net>
2013-07-07 01:43:21 +0300

    ThreadContext moved outside ArchitectureImplementation

--- packages/hal/architecture/synthetic/posix/include/ArchitectureImplementation.h
+++ packages/hal/architecture/synthetic/posix/include/ArchitectureImplementation.h
@@ -63,130 +63,95 @@ namespace hal
       putGreeting(void);
 #endif
 
+      static void
+      yield(void);
+
       /// @} end of name Public member functions
 
-      // ======================================================================
+    };
+
+    // ========================================================================
 
 #pragma GCC diagnostic push
 #if defined(__clang__)
 #pragma clang diagnostic ignored "-Wpadded"
 #endif
 
-      /// \headerfile ArchitectureImplementation.h "hal/architecture/synthetic/posix/include/ArchitectureImplementation.h"
-      /// \ingroup posix
-      /// \nosubgrouping
+    /// \headerfile ArchitectureImplementation.h "hal/architecture/synthetic/posix/include/ArchitectureImplementation.h"
+    /// \ingroup posix
+    /// \nosubgrouping
+    ///
+    /// \brief POSIX context handling.
+    ///
+    /// \details
+    /// This class provides generic POSIX context handling support.
+    class ThreadContext
+    {
+
+    public:
+      /// \name Constructors/destructor
+      /// @{
+
+      /// \brief Constructor.
+      ThreadContext(void);
+
+      /// @} end of name Constructors/destructor
+
+      /// \name Public member functions
+      /// @{
+
+      /// \brief Create the initial context in the local storage.
       ///
-      /// \brief POSIX context handling.
+      /// \param [in] pStackBottom    Pointer to the first stack element.
+      /// \param [in] stackSize       Size of stack in elements.
+      /// \param [in] entryPoint      Pointer to thread code.
+      /// \param [in] pParameters     Pointer to thread parameters.
+      /// \par Returns
+      ///    Nothing.
+      void
+      create(hal::arch::stackElement_t* pStackBottom,
+          hal::arch::stackSize_t stackSize,
+          os::core::threadEntryPoint_t entryPoint, void* pParameters);
+
+      /// \brief Save the current context in the local storage.
       ///
-      /// \details
-      /// This class provides generic POSIX context handling support.
-      class ThreadContext
-      {
+      /// \par Parameters
+      ///    None.
+      /// \par Returns
+      ///    Nothing.
+      void
+      save(void);
 
-      public:
-        /// \name Constructors/destructor
-        /// @{
+      /// \brief Restore the current context from the local storage.
+      ///
+      /// \par Parameters
+      ///    None.
+      /// \par Returns
+      ///    Nothing.
+      void
+      restore(void);
 
-        /// \brief Constructor.
-        ThreadContext(void);
+      /// @} end of name Public member functions
 
-        /// @} end of name Constructors/destructor
+    private:
+      /// \brief The context storage.
+      ucontext_t m_context;
 
-        /// \name Public member functions
-        /// @{
-
-        /// \brief Create the initial context in the local storage.
-        ///
-        /// \param [in] pStackBottom    Pointer to the first stack element.
-        /// \param [in] stackSize       Size of stack in elements.
-        /// \param [in] entryPoint      Pointer to thread code.
-        /// \param [in] pParameters     Pointer to thread parameters.
-        /// \par Returns
-        ///    Nothing.
-        void
-        create(hal::arch::stackElement_t* pStackBottom,
-            hal::arch::stackSize_t stackSize,
-            os::core::threadEntryPoint_t entryPoint, void* pParameters);
-
-        /// \brief Save the current context in the local storage.
-        ///
-        /// \par Parameters
-        ///    None.
-        /// \par Returns
-        ///    Nothing.
-        void
-        save(void);
-
-        /// \brief Restore the current context from the local storage.
-        ///
-        /// \par Parameters
-        ///    None.
-        /// \par Returns
-        ///    Nothing.
-        void
-        restore(void);
-
-        /// @} end of name Public member functions
-
-      private:
-        /// \brief The context storage.
-        ucontext_t m_context;
-
-        /// \brief The C error indicator storage.
-        int m_error;
-      };
+      /// \brief The C error indicator storage.
+      int m_error;
     };
 
 #pragma GCC diagnostic pop
-
-    // ========================================================================
 
     /// \details
     /// Used only to explicitly initialise m_error.
     inline
     __attribute__((always_inline))
-    ArchitectureImplementation::ThreadContext::ThreadContext(void)
+    ThreadContext::ThreadContext(void)
     {
       m_error = 0;
     }
 
-    /// \details
-    /// Preserve errno and get context.
-    inline void
-    __attribute__((always_inline))
-    ArchitectureImplementation::ThreadContext::save(void)
-    {
-      m_error = errno;
-
-#pragma GCC diagnostic push
-#if defined(__clang__)
-#pragma clang diagnostic ignored "-Wdeprecated-declarations"
-#endif
-
-      getcontext(&m_context);
-
-#pragma GCC diagnostic pop
-
-    }
-
-    /// \details
-    /// Restore context and errno.
-    inline void
-    __attribute__((always_inline))
-    ArchitectureImplementation::ThreadContext::restore(void)
-    {
-      errno = m_error;
-
-#pragma GCC diagnostic push
-#if defined(__clang__)
-#pragma clang diagnostic ignored "-Wdeprecated-declarations"
-#endif
-
-      setcontext(&m_context);
-
-#pragma GCC diagnostic pop
-
-    }
 
   // ========================================================================
 
@@ -197,6 +162,7 @@ namespace hal
   namespace arch
   {
     typedef hal::posix::ArchitectureImplementation ArchitectureImplementation;
+    typedef hal::posix::ThreadContext ThreadContext;
   }
 
 // ========================================================================
