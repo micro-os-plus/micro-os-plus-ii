9d11db1b2005e50ac782be54e95a7db23620171e
Liviu Ionescu <ilg@livius.net>
2013-07-29 00:09:27 +0300

    moved to os::core

--- packages/portable/language/cpp/include/malloc.h
+++ packages/portable/language/cpp/include/malloc.h
@@ -1,19 +1,36 @@
+//
+// This file is part of the µOS++ distribution.
+// Copyright (c) 2013 Liviu Ionescu.
+//
+// [Partly inspired from the LLVM libcxx sources].
+// Copyright (c) 2009-2013 by the contributors listed in
+// 'LLVM libcxx Credits.txt'. See 'LLVM libcxx License.txt' for details.
+//
+// References are to ISO/IEC 14882:2011(E) Third edition (2011-09-01).
+//
+
+/// \file
+/// \brief Local malloc() & free() declarations.
+
 #ifndef OS_PORTABLE_LANGUAGE_CPP_INCLUDE_MALLOC_H
 #define OS_PORTABLE_LANGUAGE_CPP_INCLUDE_MALLOC_H
 
 #include "portable/core/include/ConfigDefines.h"
 
-#include <stdint.h>
-#include <stddef.h>
+#include "portable/language/cpp/include/cstdint.h"
+#include "portable/language/cpp/include/cstddef.h"
 
 namespace os
 {
+  namespace core
+  {
+    void*
+    malloc(os::std::size_t size);
 
-  void*
-  malloc(size_t size);
+    void
+    free(void *ptr);
 
-  void
-  free(void *ptr);
-}
+  } // namespace core
+} // namespace os
 
 #endif // OS_PORTABLE_LANGUAGE_CPP_INCLUDE_MALLOC_H
--- packages/portable/language/cpp/src/malloc.cpp
+++ packages/portable/language/cpp/src/malloc.cpp
@@ -1,36 +1,53 @@
-#include "portable/language/cpp/include/malloc.h"
+//
+// This file is part of the µOS++ distribution.
+// Copyright (c) 2013 Liviu Ionescu.
+//
+// [Partly inspired from the LLVM libcxx sources].
+// Copyright (c) 2009-2013 by the contributors listed in
+// 'LLVM libcxx Credits.txt'. See 'LLVM libcxx License.txt' for details.
+//
+// References are to ISO/IEC 14882:2011(E) Third edition (2011-09-01).
+//
+
+/// \file
+/// \brief Local malloc() & free() declarations.
+
+#include "portable/core/include/ConfigDefines.h"
 
 #include "portable/diagnostics/include/Trace.h"
 
-//#include <stddef.h>
-#include <stdlib.h>
+#include "portable/language/cpp/include/malloc.h"
+
+#include "portable/language/cpp/include/cstdlib.h"
 
 namespace os
 {
-
-  void*
-  malloc(size_t size)
+  namespace core
   {
-    void* p = ::malloc(size);
+    void*
+    malloc(os::std::size_t size)
+    {
+      void* p = ::malloc(size);
 #if defined(DEBUG)
-    os::diag::trace.putString("os::malloc(");
-    os::diag::trace.putDec(size);
-    os::diag::trace.putString(") -> ");
-    os::diag::trace.putHex(p);
-    os::diag::trace.putNewLine();
+      os::diag::trace.putString("os::core::malloc(");
+      os::diag::trace.putDec(size);
+      os::diag::trace.putString(") -> ");
+      os::diag::trace.putHex(p);
+      os::diag::trace.putNewLine();
 #endif
-    return p;
-  }
+      return p;
+    }
 
-  void
-  free(void *ptr)
-  {
+    void
+    free(void *ptr)
+    {
 #if defined(DEBUG)
-    os::diag::trace.putString("os::free(");
-    os::diag::trace.putHex(ptr);
-    os::diag::trace.putString(")");
-    os::diag::trace.putNewLine();
+      os::diag::trace.putString("os::core::free(");
+      os::diag::trace.putHex(ptr);
+      os::diag::trace.putString(")");
+      os::diag::trace.putNewLine();
 #endif
-    return ::free(ptr);
-  }
-}
+      return ::free(ptr);
+    }
+  } // namespace core
+} // namespace os
