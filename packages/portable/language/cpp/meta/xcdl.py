b6fa86b8a3f3bbf8422fcbf44a2bb82b0381c6c3
Liviu Ionescu <ilg@livius.net>
2013-07-29 00:10:26 +0300

    abort.cpp added

--- packages/portable/language/cpp/meta/xcdl.py
+++ packages/portable/language/cpp/meta/xcdl.py
@@ -26,10 +26,36 @@ Package(
                 'exception.cpp',
                 'new.cpp',
                 'malloc.cpp',
+                'abort.cpp',
             ],
 
             headerDefinition='OS_INCLUDE_PORTABLE_LANGUAGE_CPP_STREAMS',
               
         ),
+              
+        Component(
+            id='component.os.portable.language.cpp.exceptions',
+            name='C++ language exceptions support',
+            description='Support for C++ exceptions.',
+            
+            isEnabled=False,                      
+
+            headerDefinition='OS_INCLUDE_PORTABLE_LANGUAGE_CPP_EXCEPTIONS',
+            
+            children=[
+                Option(
+                    id='component.os.portable.language.cpp.exceptions.handlers',
+                    name='C++ language exceptions handlers support',
+                    description='Support for C++ exception handlers.',
+
+                    isEnabled=False,                      
+
+                    headerDefinition='OS_INCLUDE_PORTABLE_LANGUAGE_CPP_EXCEPTIONS_HANDLERS',
+                    
+                ),
+                
+            ],
+        ),
+              
     ],
 )
