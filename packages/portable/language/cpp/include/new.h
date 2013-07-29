7e8bded138975e0ca7d8770fc527a91d69f031e4
Liviu Ionescu <ilg@livius.net>
2013-07-29 16:08:45 +0300

    doxy code added

--- packages/portable/language/cpp/include/new.h
+++ packages/portable/language/cpp/include/new.h
@@ -23,30 +23,77 @@ namespace os
   namespace std
   {
 
-#if defined(OS_INCLUDE_PORTABLE_LANGUAGE_CPP_EXCEPTIONS)
+#if defined(OS_INCLUDE_PORTABLE_LANGUAGE_CPP_EXCEPTIONS) || defined(__DOXYGEN__)
 
+    /// \class bad_alloc malloc.h "portable/language/cpp/include/new.h"
+    /// \ingroup std_dynmem
+    /// \nosubgrouping
+    ///
+    /// \brief bad_alloc exception
+    ///
+    /// \details
+    /// The class bad_alloc defines the type of objects thrown as
+    /// exceptions by the implementation to report a failure to
+    /// allocate storage.
     class bad_alloc : public exception
       {
       public:
-        bad_alloc() noexcept;
+        /// \name Constructors/destructor
+        /// @{
 
+        /// \brief Constructor.
+        bad_alloc(void) noexcept;
+
+        /// \brief Destructor.
         virtual
         ~bad_alloc() noexcept;
 
+        /// @} end of name Constructors/destructor
+
+        /// \name Public member functions
+        /// @{
+
         virtual const char*
-        what() const noexcept;
+        what(void) const noexcept;
+
+        /// @} end of Public member functions
+
       };
 
+    /// \class bad_array_new_length malloc.h "portable/language/cpp/include/new.h"
+    /// \ingroup std_dynmem
+    /// \nosubgrouping
+    ///
+    /// \brief bad_array_new_length exception
+    ///
+    /// \details
+    /// The class bad_array_new_length defines the type of objects
+    /// thrown as exceptions by the implementation to report an
+    /// attempt to allocate an array of size less than zero or greater
+    /// than an implementation-defined limit (5.3.4).
     class bad_array_new_length : public bad_alloc
       {
       public:
-        bad_array_new_length() noexcept;
+        /// \name Constructors/destructor
+        /// @{
 
+        /// \brief Constructor.
+        bad_array_new_length(void) noexcept;
+
+        /// \brief Destructor.
         virtual
         ~bad_array_new_length() noexcept;
 
+        /// @} end of name Constructors/destructor
+
+        /// \name Public member functions
+        /// @{
+
         virtual const char*
-        what() const noexcept;
+        what(void) const noexcept;
+
+        /// @} end of Public member functions
+
       };
 
 #endif
@@ -57,33 +104,61 @@ namespace os
     __throw_bad_alloc() __attribute__((noreturn));
 #endif
 
+    /// \addtogroup std_dynmem
+    /// @{
+
     struct nothrow_t
     {
     };
 
     extern const nothrow_t nothrow;
 
-#if defined(OS_INCLUDE_PORTABLE_LANGUAGE_CPP_EXCEPTIONS_HANDLERS)
+#if defined(OS_INCLUDE_PORTABLE_LANGUAGE_CPP_EXCEPTIONS_HANDLERS) || defined(__DOXYGEN__)
 
+  /// \brief Pointer to new handler function.
+  ///
+  /// \details
+  /// The type of a handler function to be called by operator new()
+  /// or operator new[]() (18.6.1) when they cannot satisfy a request
+  /// for additional storage.
+  ///
+  /// The new_handler shall perform one of the following:
+  /// - make more storage available for allocation and then return;
+  /// - throw an exception of type bad_alloc or a class derived from bad_alloc;
+  /// - terminate execution of the program without returning to the caller;
   typedef void
-  (*new_handler)();
+  (*new_handler)(void);
 
+  /// \brief Set handler for new allocations.
+  ///
+  /// \param [in] new_p         The new handler.
+  /// \return                   The previous handler.
+  ///
+  /// \details
+  /// Establishes the function designated by new_p as the current new_handler.
+  /// The initial handler is a null pointer.
   new_handler
-  set_new_handler(new_handler) noexcept;
+  set_new_handler(new_handler new_p) noexcept;
 
+  /// \brief Get handler for new allocations.
+  /// \par Parameters
+  ///    None.
+  /// \return The current pointer to the handler.
   new_handler
-  get_new_handler() noexcept;
+  get_new_handler(void) noexcept;
 
 #endif
 
+  /// @} end of addtogroup std_dynmem
+
 }
- // namespace std
-}// namespace os
+  // namespace std
+} // namespace os
 
 // skip when already included in POSIX environments
 #if !defined(_NEW) || defined(__DOXYGEN__)
 
-/// \addtogroup std_support
+/// \addtogroup std_dynmem
 /// @{
 
 /// \brief Single object allocation.
@@ -196,7 +271,7 @@ operator delete[](void*, void*) noexcept
 {
 }
 
-/// @} end of addtogroup std_support
+/// @} end of addtogroup std_dynmem
 
 #endif // !defined(_NEW)
 #endif  // OS_PORTABLE_LANGUAGE_CPP_INCLUDE_NEW_H
