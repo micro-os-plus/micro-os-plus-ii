0d329406fa9af0bc4c09caeea822d211768cc26b
Liviu Ionescu <ilg@livius.net>
2013-07-31 19:31:37 +0300

    macro assert_always() added

--- packages/portable/language/cpp/include/cassert.h
+++ packages/portable/language/cpp/include/cassert.h
@@ -37,6 +37,8 @@
 /// \note This macro is enabled only on debug configurations.
 #define assert(e) ((void) ((e) ? 0 : (os::infra::assertFailed(__func__, __FILE__, __LINE__, #e), 0)))
 
+#define assert_always(msg) ((void) ((os::infra::assertFailed(__func__, __FILE__, __LINE__, #msg), 0)))
+
 namespace os
 {
   namespace infra
@@ -48,7 +50,10 @@ namespace os
 }
 
 #else
+
 #define assert(e) ((void)0)
+#define assert_always(msg) ((void)0)
+
 #endif
 
 #endif  // OS_PORTABLE_LANGUAGE_CPP_INCLUDE_CASSERT_H_
