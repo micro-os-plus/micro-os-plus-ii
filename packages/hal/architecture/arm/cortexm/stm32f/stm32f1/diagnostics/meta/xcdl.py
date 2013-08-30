52b0e920e3ea40c1ab0f8956f6499dd7257e558d
Liviu Ionescu <ilg@livius.net>
2013-08-30 22:03:38 +0300

    update requirements to gpio and portable i2c

--- packages/hal/architecture/arm/cortexm/stm32f/stm32f1/diagnostics/meta/xcdl.py
+++ packages/hal/architecture/arm/cortexm/stm32f/stm32f1/diagnostics/meta/xcdl.py
@@ -15,10 +15,17 @@ Component(
     children=[
               
         Component(
-            id='component.os.hal.architecture.arm.cortexm.stm32f1.diagnostics.trace',
+            id='component.os.hal.architecture.arm.cortexm.stm32f1.diagnostics.trace.i2c',
             name='ST Micro STM32F10X trace implementation',
             description='Implementation to output data from the trace class.',
             
+            requirements=[
+                # we also need gpio  
+                'enable("component.os.hal.architecture.arm.cortexm.stm32f.stm32f10x.lib.stm.gpio")',              
+                # and the i2c trace implementation
+                'enable("component.os.portable.diagnostics.trace.i2c")',                           
+            ],
+                  
             headerDefinition='OS_INCLUDE_HAL_ARCHITECTURE_ARM_CORTEXM_STM32F_STM32F1_DIAGNOSTICS_TRACEIMPLEMENTATION',
         
             implements=[
