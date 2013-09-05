9e27b957d24317e82153b36a3b266b557b3ee998
Liviu Ionescu <ilg@livius.net>
2013-09-05 13:56:27 +0300

    TestSuiteImplementationI2C.h added

--- packages/portable/infrastructure/include/TestSuite.h
+++ packages/portable/infrastructure/include/TestSuite.h
@@ -21,6 +21,10 @@
 
 #include "hal/architecture/arm/cortexm/qemu/infrastructure/include/TestSuiteImplementation.h"
 
+#elif defined(OS_INCLUDE_HAL_ARCHITECTURE_ARM_CORTEXM_INFRASTRUCTURE_TESTSUITEIMPLEMENTATIONI2C)
+
+#include "hal/architecture/arm/cortexm/infrastructure/include/TestSuiteImplementationI2C.h"
+
 #elif defined(OS_INCLUDE_HAL_ARCHITECTURE_INFRASTRUCTURE_TESTSUITEIMPLEMENTATION_CUSTOM)
 
 #include "hal/architecture/infrastructure/include/TestSuiteImplementation.h"
