910e8ee01caa5acfb9edc29f00188ce23ffe218e
Liviu Ionescu <ilg@livius.net>
2013-09-05 13:24:42 +0300

    double word stack alignment added

--- packages/hal/architecture/arm/cortexm/include/ArchitectureImplementation.h
+++ packages/hal/architecture/arm/cortexm/include/ArchitectureImplementation.h
@@ -19,6 +19,8 @@
 #include "hal/architecture/arm/cortexm/include/ArchitectureDefinitions.h"
 
 #include "hal/architecture/arm/cortexm/include/TimerTicksImplementation.h"
+#include "hal/architecture/arm/cortexm/include/ThreadContext.h"
+#include "hal/architecture/arm/cortexm/include/InterruptsCriticalSection.h"
 
 #if defined(OS_INCLUDE_HAL_MCU_FAMILY_STM32F)
 
@@ -44,9 +46,6 @@
 
 #endif
 
-//#include "portable/core/include/Stack.h"
-
-#define OS_DEBUG_THREADCONTEXT  (1)
 
 namespace hal
 {
@@ -138,11 +137,6 @@ namespace hal
       static void
       resetWatchdog(void);
 
-      static void
-      contextSwitch(void);
-
-      static hal::arch::stackElement_t** ms_ppStack;
-
       /// @} end of name Public member functions
 
       //ThreadContext context;
@@ -156,6 +150,10 @@ namespace hal
     __attribute__((always_inline))
     ArchitectureImplementation::initialiseSystem(void)
     {
+      // Double word stack alignment
+      (*((volatile unsigned long *) 0xe000ed14)) |=
+          (1 << 9);
+
       hal::cortexm::FamilyImplementation::initialiseSystem();
     }
 
@@ -189,171 +187,6 @@ namespace hal
       __asm volatile( " wfi     \n" );
     }
 
-    // ========================================================================
-
-    /// \headerfile ArchitectureImplementation.h "hal/architecture/arm/cortexm/include/ArchitectureImplementation.h"
-    /// \ingroup arm_cm
-    /// \nosubgrouping
-    ///
-    /// \brief Cortex-M context handling.
-    ///
-    /// \details
-    /// This class provides generic Cortex-M context handling support.
-    class ThreadContext
-    {
-    public:
-      /// \name Constructors/destructor
-      /// @{
-
-      /// \brief Default constructor.
-      ThreadContext(void);
-      ~ThreadContext(void);
-
-      /// @} end of name Constructors/destructor
-
-      /// \name Public member functions
-      /// @{
-
-#if 0
-      /// \brief Create the initial context in the stack object.
-      ///
-      /// \param [in] pStackTop Pointer to the last stack element.
-      /// \param [in] entryPoint Pointer to thread code.
-      /// \param [in] pParameters Pointer to thread parameters.
-      /// \par Returns
-      ///    Nothing.
-      static hal::arch::stackElement_t*
-      createInitial(hal::arch::stackElement_t* pStackTop,
-          os::core::threadEntryPoint_t entryPoint, void* pParameters);
-#endif
-
-      /// \brief Create the initial context in the local storage.
-      ///
-      /// \param [in] pStackBottom    Pointer to the first stack element.
-      /// \param [in] stackSizeBytes  Size of stack in bytes.
-      /// \param [in] trampolineEntryPoint      Pointer to trampoline code.
-      /// \param [in] p1              First parameter.
-      /// \param [in] p2              Second parameter.
-      /// \param [in] p3              Third parameter.
-      /// \par Returns
-      ///    Nothing.
-      void
-      create(hal::arch::stackElement_t* pStackBottom,
-          hal::arch::stackSize_t stackSizeBytes,
-          os::core::trampoline3_t trampolineEntryPoint, void* p1, void* p2,
-          void* p3);
-
-#if 0
-      /// \brief Save the current context on the stack.
-      ///
-      /// \par Parameters
-      ///    None.
-      /// \par Returns
-      ///    Nothing.
-      static void
-      save(void);
-#endif
-
-      /// \brief Save the current context in the local storage.
-      ///
-      /// \par Parameters
-      ///    None.
-      /// \retval true  Context saved, returns for the first time
-      /// \retval false Context restored, returns for the second time
-      ///    Nothing.
-      bool
-      save(void);
-
-      /// \brief Restore the current context from the stack.
-      ///
-      /// \par Parameters
-      ///    None.
-      /// \par Returns
-      ///    Nothing.
-      void
-      restore(void);
-
-      hal::arch::stackElement_t**
-      getPPStack(void);
-
-      /// @} end of name Public member functions
-
-    private:
-
-      hal::arch::stackElement_t* m_pStack;
-    };
-
-    // ------------------------------------------------------------------------
-
-    /// \details
-    /// TBD
-    inline hal::arch::stackElement_t**
-    __attribute__((always_inline))
-    ThreadContext::getPPStack(void)
-    {
-      return &m_pStack;
-    }
-
-    /// \details
-    /// TBD
-    inline bool
-    __attribute__((always_inline))
-    ThreadContext::save(void)
-    {
-      // TBD
-      return true;
-    }
-
-    /// \details
-    /// TBD
-    inline void
-    __attribute__((always_inline))
-    ThreadContext::restore(void)
-    {
-      // TBD
-    }
-
-    // ========================================================================
-
-    /// \headerfile ArchitectureImplementation.h "hal/architecture/arm/cortexm/include/ArchitectureImplementation.h"
-    /// \ingroup arm_cm
-    /// \nosubgrouping
-    ///
-    /// \brief Implementation of the interrupts critical section.
-    ///
-    /// \details
-    /// This class is used to create a critical section where the timer ticks
-    /// are disabled.
-    /// It uses the RAII paradigm, i.e. it blocks the signals in
-    /// the constructor and restores them to the initial state in the
-    /// destructor.
-    ///
-    /// Embedded calls are allowed, without other depth limitations other
-    /// than the available stack.
-    class InterruptsCriticalSection
-    {
-    public:
-      /// \name Constructors/destructor
-      /// @{
-
-      /// \brief Constructor
-      InterruptsCriticalSection(void);
-
-      /// \brief Destructor
-      ~InterruptsCriticalSection();
-
-      /// @} end of name Constructors/destructor
-
-    private:
-      /// \name Private member variables
-      /// @{
-
-      basepri_t m_basepri;
-
-      /// @} end of Private member variables
-
-    };
-
   // ==========================================================================
 
   }// namespace cortexm
@@ -364,14 +197,6 @@ namespace hal
   {
     /// \brief Generic ArchitectureImplementation, pointing to specific one.
     using ArchitectureImplementation = hal::cortexm::ArchitectureImplementation;
-
-#if defined(OS_INCLUDE_PORTABLE_CORE_SCHEDULER) || defined(__DOXYGEN__)
-
-    using ThreadContext = hal::cortexm::ThreadContext;
-    using TimerTicksImplementation = hal::cortexm::TimerTicksImplementation;
-    using InterruptsCriticalSection = hal::cortexm::InterruptsCriticalSection;
-
-#endif // defined(OS_INCLUDE_PORTABLE_CORE_SCHEDULER)
   }
 
 // ==========================================================================
